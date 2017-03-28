fccbc69bc0578bc25ec2bad8d191fca203404306
resakemal <resakemal@gmail.com>
2017-03-28 16:46:51 +0700

    Board, Player Objects & Logic, Tile Struct

---
+++ Battle of the Ark/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public TileController tileControl;
+	private LinkedList<Tile> btiles;
+	private Tile pos;
+	private Tile laspos;
+	private bool teleport;
+	// Use this for initialization
+	void Start () {
+		btiles = tileControl.tiles;
+		teleport = false;
+		pos = new Tile("T1");
+		laspos = new Tile("T1");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void teleportPlayer (string tile) {
+		
+	}
+
+	void movePlayer (int steps) {
+		bool dir = false;
+		LinkedListNode<Tile> curTile = btiles.Find (pos);
+		if (pos.tileID == laspos.tileID || teleport) {
+			dir = chooseDirection (curTile);
+			steps--;
+		} else {
+			if (Equals(curTile.Previous.Value,laspos))
+				dir = true;
+			else
+				dir = false;
+		}
+		GameObject nextTileObj;
+		int i = 1;
+		while (i <= steps) {
+			if (curTile.Value.intersection)
+				dir = chooseDirection (curTile);
+			else {
+				laspos = curTile.Value;
+				if (dir)
+					curTile = curTile.Next;
+				else
+					curTile = curTile.Previous;
+				pos = curTile.Value;
+				nextTileObj = GameObject.Find (curTile.Value.tileID);
+				this.transform.position = nextTileObj.transform.position;
+			}
+			i++;
+		}
+	}
+
+	bool chooseDirection(LinkedListNode<Tile> current) {
+		bool direction = false;
+		List<string> choice = current.Value.branch;
+		laspos = current.Value;
+		//show option to player
+		if (Input.GetKeyDown ("j")) {
+			foreach (Tile t in btiles) {
+				if (t.tileID == choice [0]) {
+					current = btiles.Find (t);
+					direction = false;
+				}
+			}
+		} else if (Input.GetKeyDown ("k")) {
+			foreach (Tile t in btiles) {
+				if (t.tileID == choice [1]) {
+					current = btiles.Find (t);
+					direction = true;
+				}
+			}
+		}
+		pos = current.Value;
+		return direction;
+	}
+}
---
+++ Battle of the Ark/Assets/Scripts/TileController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Tile {
+	public string tileID;
+	public bool intersection;
+	public List<string> branch;
+
+	public Tile(string ID) {
+		tileID = ID;
+		intersection = false;
+		branch = null;
+	}
+
+	public Tile(string ID, List<string> branchlist) {
+		tileID = ID;
+		intersection = true;
+		branch = branchlist;
+	}
+
+	public bool Equals(Tile t1, Tile t2) {
+		if (t1.tileID == t2.tileID) {
+			if (t1.intersection == t2.intersection)
+				return true;
+			else
+				return false;
+		} else
+			return false;
+	}
+}
+
+public class TileController : MonoBehaviour {
+	public LinkedList<Tile> tiles;
+	// Use this for initialization
+	void Start () {
+		Tile t1 = new Tile ("T1");
+		Tile t2 = new Tile ("T2");
+		Tile t3 = new Tile ("T3");
+		Tile t4 = new Tile ("T4");
+		tiles.AddLast (t1);
+		tiles.AddLast (t2);
+		tiles.AddLast (t3);
+		tiles.AddLast (t4);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
